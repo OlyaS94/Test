30c04f07409b34e0a2a50f63c7503cd9eac48bf8
Olga Smirnova <olya_fcsm@mail.ru>
2021-09-15 21:13:19 +0300

    first

---
+++ YaTest.py
@@ -0,0 +1,25 @@
+import requests
+import unittest
+from yadisk import YaFolderCreator
+
+
+class TestYaDisk(unittest.TestCase):
+  def setUp(self):
+    self.uploader = YaFolderCreator('AQAAAABE_kNnAADLW-5MFTceh0WFpUfovOhDDvQ')
+
+  def test_create_folder(self):
+    directory = 'PyTest'
+    self.assertEqual(self.uploader.create_folder(directory).status_code, 201)
+    folders_resp = requests.get("https://cloud-api.yandex.net/v1/disk/resources",
+    params={"path": '/'},
+    headers={"Authorization": f'OAuth {self.uploader.token}'})
+
+        
+    folders_list = [f['name'] for f in folders_resp.json().get('_embedded').get('items') if f['type'] == 'dir']
+    self.assertIn(directory, folders_list)
+
+  def tearDown(self):
+    del self.uploader
+    
+if __name__ == '__main__':
+  unittest.main()
